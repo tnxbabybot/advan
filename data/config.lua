dd9a319dc5f276d9b0a1d2e078f4634beea4d687
NoFooZiTM-Samiyar-Bang <36511247+NoFooZiTM@users.noreply.github.com>
2018-02-16 00:18:42 +0330

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+	plug = {
+    "TD"
+  },
+  Helper = "HELPER_CLI_BOT",
+  Token = "463320595:AAH1KYjoRa5D2gle0_L8-w-BRkzsEcpTC68",
+  bot_owner = {
+    435014771,
+    437978064
+  },
+  cmd = "^[/!#]",
+  sudo_users = {
+    437978064
+  }
+}
+return _
+end
